44decc5e40bed7c5c0f2d2d977893295edc66a66
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-01-21 01:56:58 +0600

    Add files via upload

---
+++ Loop/for_.java
@@ -0,0 +1,10 @@
+public class for_ {
+    public static void main(String[] args) {
+        for(int x=0;x<10;x++){
+        for(int z=0;z<=x;z++){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+}
+}
