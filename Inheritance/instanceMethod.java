934e1a04451b21e81ea8270f6b9057001fd240d9
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-01-22 20:24:05 +0600

    Add files via upload

---
+++ Inheritance/instanceMethod.java
@@ -0,0 +1,13 @@
+public class instanceMethod {
+    public static void main(String[] args) {
+        a obj=new a();
+        obj.instanceMethod_();
+    }
+}
+class a{
+    void instanceMethod_(){
+        System.out.println("Instance Method");
+    }
+
+  
+}
