965d5a1f28df4470d6efccc3772e54362b8890c5
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-01-25 22:56:54 +0600

    Update fileCreate.java

--- File/Create file/fileCreate.java
+++ File/Create file/fileCreate.java
@@ -4,6 +4,12 @@ public class fileCreate {
     public static void main(String[] args) {
         try {
             File file = new File("File.txt");
+            /*
+             * To create a file in a specific directory (requires permission), specify the
+             * path of the file and use
+             * double backslashes to escape the "\" character (for Windows).
+             */
+            // File file = new File("C:\\Users\\MyName\\filename.txt");
             if (file.createNewFile()) {
                 System.out.print("Created");
             } else {
